d7ca5c48421cbe2d936837e5bec4d67aa4661341
unknown <mail.ramit2499@gmail.com>
2023-02-04 18:02:57 +0530

    designed favourites section

--- src/Components/Favourites.jsx
+++ src/Components/Favourites.jsx
@@ -10,11 +10,29 @@ const Favourites = () => {
   return (
     <>
       <Navbar />
-      <section className="mt-5">
+      <section className="py-5">
         <div className="container">
-          {cart.map((get) => {
-            return <img src={get.poster} alt="image" />;
-          })}
+          <div className="row justify-content-between align-items-center">
+            {cart.map((get) => {
+              return (
+                <>
+                  <div className="col-3 mt-4">
+                    <img className="w-100" src={get.poster} alt="image" />
+                  </div>
+                  <div className="col-8">
+                    <h2>{get.title}</h2>
+                    <div className="d-flex">
+                      <h4>{get.year}</h4>
+                      <h4 className="ms-4">
+                        <em>{get.genre}</em>
+                      </h4>
+                    </div>
+                    <h6 className="mt-1">{get.plot}</h6>
+                  </div>
+                </>
+              );
+            })}
+          </div>
         </div>
       </section>
     </>
